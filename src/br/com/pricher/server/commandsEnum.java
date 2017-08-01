8df899514292b2755671bc81c22ce28488cbe184
Jeferson Machado <jefersonmachado@univille.br>
2017-07-31 21:47:17 -0300

    COMMANDS

---
+++ src/br/com/pricher/server/commandsEnum.java
@@ -0,0 +1,11 @@
+package br.com.pricher.server;
+
+/**
+ * Created by Jeferson Machado on 31/07/2017.
+ */
+public class commandsEnum {
+
+    public static final int DESLIGAR_PC = 1;
+    public static final int MSG = 2;
+    public static final int ABRIR_PORNHUB = 3;
+}
