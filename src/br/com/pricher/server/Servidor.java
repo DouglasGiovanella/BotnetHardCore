0ef8f5e65b621947fdf1e8d4a641022a2ba260e3
Douglas Giovanella <douglas_giovanella@hotmail.com>
2017-08-01 12:42:08 -0300

    xablau

--- src/br/com/pricher/server/Servidor.java
+++ src/br/com/pricher/server/Servidor.java
@@ -5,30 +5,25 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Scanner;
 
 /**
  * Created by Jeferson Machado on 26/07/2017.
  */
 public class Servidor extends Thread {
 
-    private static ArrayList<BufferedWriter> clientes;
-    private static ServerSocket server;
-    private static List<String> clientsNames;
-    private String nome;
+    private static ArrayList<BufferedWriter> clients;
+    private static HashMap<BufferedWriter, String> clientsNames;
+    private String name;
     private Socket con;
-    private InputStream in;
-    private InputStreamReader inr;
     private BufferedReader bfr;
 
-    public Servidor(Socket con) {
+    private Servidor(Socket con) {
         this.con = con;
         try {
-            //InputStream
-            in = con.getInputStream();
-            //InputStreamReader
-            inr = new InputStreamReader(in);
-            //BufferedReader
+            InputStream in = con.getInputStream();
+            InputStreamReader inr = new InputStreamReader(in);
             bfr = new BufferedReader(inr);
         } catch (IOException e) {
             e.printStackTrace();
@@ -44,19 +39,18 @@ public class Servidor extends Thread {
             OutputStream outPutStream = this.con.getOutputStream();
             Writer writer = new OutputStreamWriter(outPutStream);
             BufferedWriter bufferedWriter = new BufferedWriter(writer);
-            clientes.add(bufferedWriter);
-            nome = msg = bfr.readLine();
-            System.out.println("Cliente conectado: " + nome);
-            //clientsNames.add(nome);
-            //showConnectedClients();
+            clients.add(bufferedWriter);
+            name = msg = bfr.readLine();
+            clientsNames.put(bufferedWriter, name);
+            System.out.println("Cliente conectado: " + name);
+            showMenu();
 
             while (!"Sair".equalsIgnoreCase(msg) && msg != null) {
                 msg = bfr.readLine();
                 sendToAll(bufferedWriter, msg);
                 System.out.println(msg);
+                //TODO RETIRAR CLIENT DO ARRAY
             }
-
-
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -71,10 +65,10 @@ public class Servidor extends Thread {
     public void sendToAll(BufferedWriter bwSaida, String msg) throws IOException {
         BufferedWriter bws;
 
-        for (BufferedWriter bw : clientes) {
+        for (BufferedWriter bw : clients) {
             bws = bw;
             if (!(bwSaida == bws)) {
-                bw.write(nome + " -> " + msg + "\r\n");
+                bw.write(name + " -> " + msg + "\r\n");
                 bw.flush();
             }
         }
@@ -85,18 +79,16 @@ public class Servidor extends Thread {
      * @param args
      */
     public static void main(String[] args) {
-        clientsNames = new ArrayList<>();
         try {
-            //Cria os objetos necessários para instanciar o servidor
             JLabel lblMessage = new JLabel("Porta do Servidor:");
             JTextField txtPorta = new JTextField("8094");
             Object[] texts = {lblMessage, txtPorta};
             JOptionPane.showMessageDialog(null, texts);
-            server = new ServerSocket(Integer.parseInt(txtPorta.getText()));
 
-            final ServerSocket gambiarra = server;
+            final ServerSocket server = new ServerSocket(Integer.parseInt(txtPorta.getText()));
 
-            clientes = new ArrayList<>();
+            clientsNames = new HashMap<>();
+            clients = new ArrayList<>();
             JOptionPane.showMessageDialog(null, "Servidor ativo na porta: " + txtPorta.getText());
 
             new Thread(() -> {
@@ -104,7 +96,7 @@ public class Servidor extends Thread {
                     System.out.println("Aguardando conexão...");
                     Socket con = null;
                     try {
-                        con = gambiarra.accept();
+                        con = server.accept();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -114,27 +106,32 @@ public class Servidor extends Thread {
                 }
             }).start();
 
-            new Thread(() -> {
-                while (true) {
-
-
-                }
-            }).start();
+            new Thread(Servidor::listenCommands).start();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static void showConnectedClients() {
-        if (clientsNames.size() != 0) {
-
-            for (int i = 0; i < clientsNames.size(); i++) {
-                System.out.println("Cliente " + i + " : " + clientsNames.get(i));
-            }
-
+    private static void listenCommands() {
+        showMenu();
+        Scanner reader = new Scanner(System.in);
+        while (true) {
+            System.out.println(">. ");
+            int option = reader.nextInt();
         }
     }
 
 
+    private static void showMenu() {
+        System.out.println("-----------------------------------------");
+        if (clients.size() > 0) {
+            for (int i = 0; i < clients.size(); i++) {
+                System.out.println("(" + i + ") -> ClientName: " + clientsNames.get(clients.get(i)));
+            }
+        } else {
+            System.out.println("Sem clientes conectados");
+        }
+        System.out.println("-----------------------------------------");
+    }
 }
